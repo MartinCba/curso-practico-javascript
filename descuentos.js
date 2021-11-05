596c4053d894dffb2b0f0f158ce3f568829829e3
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2021-11-05 13:18:58 -0300

    terminada la calculadora de descuentos del taller 2

--- descuentos.js
+++ descuentos.js
@@ -3,9 +3,18 @@
 
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento;
-  const precioConDescuento =
-    (precio * porcentajePrecioConDescuento) / 100;
-    return precioConDescuento;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount(price, discount) {
+  const inputPrecio = document.getElementById("inputPrecio");
+  const precio = inputPrecio.value;
+  const inputDescuento = document.getElementById("inputDescuento");
+  const descuento = inputDescuento.value;
+  const precioConDescuento = calcularPrecioConDescuento(precio, descuento);
+  const resultado = document.getElementById("resultado");
+  return resultado.innerText = "El precio con descuento es $" + precioConDescuento;
 }
 
 /*console.log({
