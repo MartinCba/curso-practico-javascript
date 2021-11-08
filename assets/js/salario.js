d292fff858693d7c2378f33b1907c37e57d84075
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2021-11-08 12:24:05 -0300

    archivos ordenados y se agregaron los stylos para fializar el proyecto

---
+++ assets/js/salario.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push({
+  name: "Laura",
+  salary: 500,
+});
+colombia.push({
+  name: "Sofía",
+  salary: 1500,
+});
+colombia.push({
+  name: "Victoria",
+  salary: 1800,
+});
+colombia.push({
+  name: "Celeste",
+  salary: 1000,
+});
+colombia.push({
+  name: "Daniela",
+  salary: 2200,
+});
+colombia.push({
+  name: "Julieta",
+  salary: 200,
+});
+colombia.push({
+  name: "Carla",
+  salary: 500,
+});
+colombia.push({
+  name: "Mariana",
+  salary: 1500,
+});
+colombia.push({
+  name: "María",
+  salary: 1300,
+});
+colombia.push({
+  name: "Emilia",
+  salary: 2400,
+});
+colombia.push({
+  name: "Julian",
+  salary: 3400,
+});
+colombia.push({
+  name: "Martín",
+  salary: 400,
+});
+colombia.push({
+  name: "Ramón",
+  salary: 400,
+});
+colombia.push({
+  name: "Sebastián",
+  salary: 400,
+});
+colombia.push({
+  name: "Alejandro",
+  salary: 600,
+});
+colombia.push({
+  name: "Javier",
+  salary: 1600,
+});
+colombia.push({
+  name: "Luciano",
+  salary: 2600,
+});
+colombia.push({
+  name: "Marcelo",
+  salary: 1000,
+});
+colombia.push({
+  name: "Ignacio",
+  salary: 2000,
+});
+colombia.push({
+  name: "Bill Gates",
+  salary: 100000000,
+});
